193ccd79ec95ffd0736e25f8b056f8dabcf8d84d
thejsj <jorge.silva@thejsj.com>
2014-05-26 13:34:38 -0400

    Changing structure of repository to limit it to the theme

--- wp-content/themes/base/404.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <head>
-
-  </head>
-  <body>
-    <h1>Fou Oh' Fou</h1>
-  </body>
-</html>
--- wp-content/themes/base/page-login.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Template Name: Login
-*/
-?>
-<?php get_header(); ?>
-    <?php include("not-signed-in.php"); ?>
-<?php get_footer(); ?>
